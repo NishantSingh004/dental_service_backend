c6cb3e42610f52aec1f16686adaf111474aced1f
Nishant Singh <nishantpratapsingh333@gmail.com>
2025-02-08 15:29:18 +0530

    app.js updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
 const router = express.Router();
+import { User } from "../model/doctors.js";
+
 import { authenticateAdmin } from '../middleware/authMiddleware.js';
 import { addUser } from '../controllers/userController.js';
 
 router.post('/add-user', authenticateAdmin, addUser);
-
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find(); // Fetch all users from the database
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' });
+  }
+});
 export default router
